7e1f9b9589a0d7c203a788e0dae734686840fb9b
hiroshi-00 <1412hiroshi@gmail.com>
2021-05-15 14:43:17 +0000

    add verify

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,22 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  Rails.application.configure do
+    config.action_mailer.default_url_options = {
+      protocol: 'https',
+      host: ENV['DOMAIN_NAME']
+    }
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      user_name: ENV['USER_EMAIL'],
+      domain: "smtp.gmail.com",
+      password: ENV['EMAIL_PASSWORD'],
+      authentication: 'plain',
+      enable_starttls_auto: true
+    }
+  end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,22 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Rails.application.configure do
+    config.action_mailer.default_url_options = {
+      protocol: 'https',
+      host: ENV['DOMAIN_NAME']
+    }
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      user_name: ENV['USER_EMAIL'],
+      domain: "smtp.gmail.com",
+      password: ENV['EMAIL_PASSWORD'],
+      authentication: 'plain',
+      enable_starttls_auto: true
+    }
+  end
 end
