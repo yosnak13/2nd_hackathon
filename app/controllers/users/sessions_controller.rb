0d704899a6e455ccbd3f711e132a830500e2cff5
hiroshi-00 <1412hiroshi@gmail.com>
2021-05-19 20:38:16 +0900

    fix signin form

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -33,6 +33,6 @@ class Users::SessionsController < Devise::SessionsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: %i[name email password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :email, :password, :password_confirmation])
   end
 end
