44712b812065514760dc091f6a7c502435559a8b
yosnak13 <yosnak13@gmail.com>
2021-05-22 12:35:08 +0900

    define validation of post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :users
   belongs_to :stations
+  validates :user_id, presense: true
+  validates :station_id, presense: true
+  validates :comment, length: { maximum:140 }
+  validates :congestion_level, presense: true
+  validates :date, presense: true
+  validates :day_of_week, presense: true
+  validates :time, presense: true
+  validates :direction, presense: true
+
 end
