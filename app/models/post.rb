90f8688de674c1c200397b384921d013da0200b9
yosnak13 <yosnak13@gmail.com>
2021-05-23 18:47:25 +0900

    fix spelling presense to "presence"

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,14 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :station
-  validates :user_id, presense: true
-  validates :station_id, presense: true
+  validates :user_id, presence: true
+  validates :station_id, presence: true
   validates :comment, length: { maximum:140 }
-  validates :congestion_level, presense: true
-  validates :date, presense: true
-  validates :day_of_week, presense: true
-  validates :time, presense: true
-  validates :direction, presense: true
+  validates :congestion_level, presence: true
+  validates :date, presence: true
+  validates :day_of_week, presence: true
+  validates :time, presence: true
+  validates :direction, presence: true
 
   enum direction: { "西武新宿方面" => 0, "本川越方面" => 1 }
   enum congestion_level: { "着席できる" => 1, "座れないが立って乗れる" => 3, "座席の前で立てる" => 3, "立っているのがやっと" => 4, "乗り切れないほど混雑している" => 5}
