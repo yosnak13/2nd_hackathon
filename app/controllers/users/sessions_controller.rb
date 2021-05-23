d7a3afd8226dac84ca83bae9f8b4f55fb9558ee5
yosnak13 <yosnak13@gmail.com>
2021-05-23 21:44:24 +0900

    after logged_in_path is root_path

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -20,7 +20,7 @@ class Users::SessionsController < Devise::SessionsController
 
   # ログイン後のリダイレクト先
   def after_sign_in_path_for(user)
-    home_index_path
+    root_path
   end
 
   # ログアウト後のリダイレクト先
