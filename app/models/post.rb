fac6d6ff9f87bcf13ce713891ae6f5decac9a5fb
yosnak13 <yosnak13@gmail.com>
2021-05-23 20:42:21 +0900

    fix enum

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,6 @@ class Post < ApplicationRecord
   validates :time, presence: true
   validates :direction, presence: true
 
-  enum direction: { "西武新宿方面" => 0, "本川越方面" => 1 }
-  enum congestion_level: { "着席できる" => 1, "座れないが立って乗れる" => 3, "座席の前で立てる" => 3, "立っているのがやっと" => 4, "乗り切れないほど混雑している" => 5}
+  enum direction: { 0 => "西武新宿方面",  1 => "本川越方面" }
+  enum congestion_level: { 1 => "着席できる", 2 => "座れないが立って乗れる", 3 => "座席の前で立てる", 4 => "立っているのがやっと", 5 => "乗り切れないほど混雑している" }
 end
