70fc0be9968718889b14b3311ce8ec114d06fad6
yosnak13 <yosnak13@gmail.com>
2021-05-26 07:28:59 +0900

    hidden_fieldで列車種別を保存

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,8 +10,7 @@ class Post < ApplicationRecord
   validates :time, presence: true
   validates :direction, presence: true
 
-  # enum direction: { 10 => "西武新宿方面",  20 => "本川越方面" }
-  # enum congestion_level: { 1 => "着席できる", 2 => "座れないが立って乗れる", 3 => "座席の前で立てる", 4 => "立っているのがやっと", 5 => "乗り切れないほど混雑している" }
   enum direction: { "西武新宿方面" => 10,  "本川越方面" => 20 }
   enum congestion_levels: { "着席できる" => 1, "座れないが立って乗れる" => 2, "座席の前で立てる" => 3, "立っているのがやっと" => 4, "乗り切れないほど混雑している" => 5 }
+  # enum train_type: { "快速急行/Fライナー" => 0, "通勤急行" => 1, "急行" => 2, "準急" => 3, "各駅停車" => 4 }
 end
