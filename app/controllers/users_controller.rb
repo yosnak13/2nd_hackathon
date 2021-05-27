6182b80e773e84f703e95874198b90d8cc8071cc
hiroshi-00 <1412hiroshi@gmail.com>
2021-05-28 03:25:48 +0900

    add about page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, except:
+  before_action :authenticate_user!
 
   def index
     @posts = Post.all
--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
 
   root to: 'users#index'
   get 'home/index'
-  
+  get 'home/about', as: 'about'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
