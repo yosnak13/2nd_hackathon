4bb8f83d39ab215e56d3bdedb7393161d3646c58
yosnak13 <yosnak13@gmail.com>
2021-05-30 22:02:41 +0900

    train_typeのバリデーション追加、順番をformと統一

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,14 @@ class Post < ApplicationRecord
   belongs_to :station
 
   validates :user_id, presence: true
+  validates :direction, presence: true
+  validates :train_type, presence: true
   validates :station_id, presence: true
-  validates :comment, length: { maximum:140 }
-  validates :congestion_level, presence: true
   validates :date, timeliness: { on_or_before: :now, format: '%Y-%m-%d'}, allow_blank: false
   validates :day_of_week, presence: true
   validates :time, presence: true
-  validates :direction, presence: true
+  validates :congestion_level, presence: true
+  validates :comment, length: { maximum:140 }
 
   enum direction: { "西武新宿方面" => 0,  "本川越方面" => 1 }
   enum congestion_level: { "着席できる" => 1, "座れないが立って乗れる" => 2, "座席の前で立てる" => 3, "立っているのがやっと" => 4, "乗り切れないほど混雑している" => 5 }
