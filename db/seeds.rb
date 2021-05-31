a16dccb7de2232f9f63a865bca9216f47d0f4642
Kazuya-Sakashita <kz0508@gmail.com>
2021-05-31 18:30:52 +0900

    viewファイル修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,12 +28,12 @@
 # end
 
 # サンプルpost作成
-200.times do
+500.times do
   date = Faker::Date.between(from: '2021-05-19', to: '2021-05-27')
   train_type = ["快速急行/Fライナー", "通勤急行", "急行", "準急", "各駅停車"]
   day = ["日", "月", "火", "水", "木", "金", "土"]
   post = Post.create!(
-    user_id: Faker::Number.within(range: 13..15),
+    user_id: Faker::Number.within(range: 1..5),
     station_id: Faker::Number.within(range: 1..2),
     congestion_level: Faker::Number.within(range: 1..5),
     date: date,
