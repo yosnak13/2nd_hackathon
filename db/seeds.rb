c85c3cb583259ea04698ce0cacfeb8d1306b9764
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-06-02 18:10:47 +0900

    modify seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,7 +33,7 @@
   train_type = ["快速急行/Fライナー", "通勤急行", "急行", "準急", "各駅停車"]
   day = ["日", "月", "火", "水", "木", "金", "土"]
   post = Post.create!(
-    user_id: Faker::Number.within(range: 5..14),
+    user_id: Faker::Number.within(range: 1..2),
     station_id: Faker::Number.within(range: 1..2),
     congestion_level: Faker::Number.within(range: 1..5),
     date: date,
