61a0df48fab200d1d2aa0688891e746294439693
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-06-09 02:09:49 +0900

    modify index&about_view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -32,7 +32,6 @@ class HomeController < ApplicationController
     end
   end
 
-
   def post_params
     params.require(:post).permit(:comment, :congestion_level, :date, :day_of_week, :time, :direction, :train_type, :station_id)
   end
