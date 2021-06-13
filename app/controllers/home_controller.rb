1b13a68a850a4e88df951ed8631b4889f87669f3
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-06-13 14:07:42 +0900

    add icon to index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,11 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   before_action :set_q, only: [:index, :search]
+  before_action :set_scraping, only: [:index, :search]
+  before_action :set_posts, only: [:index, :search]
 
 
-  def index
-    require 'nokogiri'
-    require 'open-uri'
-    url = 'https://transit.yahoo.co.jp/traininfo/detail/86/0/'
-    doc = Nokogiri::HTML(open(url),nil,"utf-8")
-    nodes = doc.xpath('//*[@id="mdServiceStatus"]/dl/dt/text()')
-    @traffic = nodes.each { |node| pp node }
-    @posts = Post.all.includes(:user, :station).order(date: 'DESC', time: 'DESC').limit(30)
+  def index    
     @graph = Post.group(:time).order(time: 'ASC').average(:congestion_level)
   end
 
@@ -22,15 +17,8 @@ class HomeController < ApplicationController
   end
 
   def search
-    require 'nokogiri'
-    require 'open-uri'
-    url = 'https://transit.yahoo.co.jp/traininfo/detail/86/0/'
-    doc = Nokogiri::HTML(open(url),nil,"utf-8")
-    nodes = doc.xpath('//*[@id="mdServiceStatus"]/dl/dt/text()')
-    @traffic = nodes.each { |node| pp node }
     @results = @q.result
     @search = Post.where(direction:params[:q][:direction]).where(station_id:params[:q][:station_id]).where(day_of_week:params[:q][:day_of_week])
-    @posts = Post.all.includes(:user, :station).order(date: 'DESC', time: 'DESC').limit(30)
     @graph = @search.group(:time).order(time: 'ASC').average(:congestion_level)
     respond_to do |format|
       format.html { render :index }
@@ -38,9 +26,22 @@ class HomeController < ApplicationController
     end
   end
 
-  def post_params
-    params.require(:post).permit(:comment, :congestion_level, :date, :day_of_week, :time, :direction, :train_type, :station_id)
-  end
+  private
+    def post_params
+      params.require(:post).permit(:comment, :congestion_level, :date, :day_of_week, :time, :direction, :train_type, :station_id)
+    end
 
-  
+    def set_scraping
+      require 'nokogiri'
+      require 'open-uri'
+      url = 'https://transit.yahoo.co.jp/traininfo/detail/86/0/'
+      doc = Nokogiri::HTML(open(url),nil,"utf-8")
+      nodes = doc.xpath('//*[@id="mdServiceStatus"]/dl/dt/text()')
+      @traffic = nodes.each { |node| pp node }
+    end
+
+    def set_posts
+    @posts = Post.all.includes(:user, :station).order(date: 'DESC', time: 'DESC').limit(30)
+    @icon = ["混雑1.jpg", "混雑2.jpg", "混雑3.jpg", "混雑4.jpg", "混雑5.jpg"]
+    end
 end
