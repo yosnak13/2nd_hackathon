0a3680f755a4647644251ef604da943b8280b525
hiroshi-00 <1412hiroshi@gmail.com>
2021-06-17 00:08:36 +0900

    merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,18 @@ class UsersController < ApplicationController
 
   def mypost
     @user = current_user.name
-    # @posts = Post.where("user_id = #{current_user.id}" ).page(params[:page]).per(10)
-    @posts = current_user.posts.page(params[:page]).per(10)
+    @posts = Post.where("user_id = #{current_user.id}").includes(:station).order(date: 'DESC', time: 'DESC').page(params[:page]).per(100)
+    
+  end
+
+  def delete
+    @post = Post.find(params[:id])
+    if @post.delete
+      flash[:notice] = "投稿を削除しました"
+    else
+      flash[:alert] = "削除できません"
+    end
+    redirect_back fallback_location: mypost_user_path(current_user.id)
   end
 
   def favorite
